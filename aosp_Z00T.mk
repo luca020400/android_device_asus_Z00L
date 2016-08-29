38f878e0fd76a526ec056dd63827d5feca0f169a
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-08-29 09:46:44 +0200

    Z00T: aospify!
    Change-Id: I3599f9bb0c1db9dbd39f5bcb757dd39ded2032e7

---
+++ aosp_Z00T.mk
@@ -0,0 +1,8 @@
+$(call inherit-product, device/asus/Z00T/full_Z00T.mk)
+
+PRODUCT_NAME := aosp_Z00T
+PRODUCT_BRAND := asus
+PRODUCT_MANUFACTURER := asus
+PRODUCT_DEVICE := Z00T
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
