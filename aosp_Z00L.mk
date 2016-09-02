71c862e432a1a1ffd93522675cdb06b732aba2f9
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-09-02 16:35:06 +0200

    include caf.mk

--- aosp_Z00L.mk
+++ aosp_Z00L.mk
@@ -1,3 +1,7 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+$(call inherit-product, caf_manifest/caf.mk)
 $(call inherit-product, device/asus/Z00L/full_Z00L.mk)
 
 PRODUCT_NAME := aosp_Z00L
