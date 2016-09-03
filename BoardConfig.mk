f18a713cbec3314121a5bc5e75c7cda9b8a57b3c
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-09-03 16:58:26 +0200

    Z00L: Hardcode device path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,19 +16,17 @@
 # inherit from common msm8916-common
 -include device/asus/msm8916-common/BoardConfigCommon.mk
 
-DEVICE_PATH := device/asus/Z00L
-
 # Assert
 TARGET_OTA_ASSERT_DEVICE := Z00L
 
 # Board
-TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+TARGET_BOARD_INFO_FILE := device/asus/Z00L/board-info.txt
 
 # Kernel
 TARGET_KERNEL_CONFIG := cyanogenmod_Z00L_defconfig
 
 # Properties
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_SYSTEM_PROP += device/asus/Z00L/system.prop
 
 # inherit from the proprietary version
 -include vendor/asus/Z00L/BoardConfigVendor.mk
